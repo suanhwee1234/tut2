f3068d17e8876675d9040f8e4b2412967f411fc0
suanhwee <suanhwee@gmail.com>
2022-05-16 17:33:46 +0800

    tut2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 
 //tut1 
-//https://www.youtube.com/watch?v=tKzSnjWPtEw&list=PLQg6GaokU5CwiVmsZ0d_9Zsg_DnIP_xwr&index=1&ab_channel=AnthonySistilli
+//https://www.youtube.com/watch?v=tKzSnjWPtEw&list=PLQg6GaokU5CwiVmsZ0d_9Zsg_DnIP_xwr&index=2&ab_channel=AnthonySistilli
 import React from 'react';
-import Checkbox from '@material-ui/core/Checkbox';
-import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import { orange,purple } from '@material-ui/core/colors';
-import {Button,Typography} from '@material-ui/core'
-import theme from './theme';
+import { Grid } from '@material-ui/core';
 
-
-const useStyles = makeStyles((theme) => ({
-  helloThereStyle:{
-    fontStyle:'oblique',
-    color:"red",
-    fontSize:"30px"
-
-  },
-  buttonStyles:{
-    color:'green'
-  }
-}));
-
-
-export default function App() {
-  const classes = useStyles();
+const App=()=> {
+  
   return (  
-      <div className="App">
-        <Typography 
-        className={classes.helloThereStyle}
-        variant='h1' color='primary'>Hello there </Typography>
-        <Button 
-        className={classes.buttonStyles}
-      color="secondary"
-      variant="outlined">This is our first button</Button>
-     </div>
+      <Grid container direction='column'>
+        <Grid>header</Grid>
+        <Grid>content</Grid>
+      </Grid>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from '@material-ui/core/styles';
 // import theme from "./theme";
 import App from "./App";
-import theme from './theme';
+
 
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
 
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
+       
             <App />
-        </ThemeProvider>
+       
     </React.StrictMode>
 
 
