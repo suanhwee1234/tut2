8877fefc18ca0419928c53e092f939bb0334bea1
suan hwee <suanhwee@gmail.com>
2022-05-18 19:24:08 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-//tut2
-//https://www.youtube.com/watch?v=tKzSnjWPtEw&list=PLQg6GaokU5CwiVmsZ0d_9Zsg_DnIP_xwr&index=2&ab_channel=AnthonySistilli
+//tut3
+//https://www.youtube.com/watch?v=k_AN49fA9g0&list=PLQg6GaokU5CwiVmsZ0d_9Zsg_DnIP_xwr&index=3&ab_channel=AnthonySistilli
 import React from "react";
-import { Grid } from "@material-ui/core";
-import Header from "./header";
-import AcUnitIcon from "@material-ui/icons/AcUnit";
-import Content from "./Content";
+import { makeStyles } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
-const App = () => {
-  return (
-    <Grid container direction="column">
-      <Grid item>
-        <Header />
-      </Grid>
-      <Grid item container>
-        <Grid xs={false} sm={2} />
-        <Grid xs={2} sm={8}>
-          <Content />
-        </Grid>
-        <Grid xs={false} sm={2} />
-      </Grid>
-    </Grid>
-  );
+const useStyles=makeStyles({
+buttonStyle:{
+  color:'red',
+  border:'none'
+}
+});
+
+export default function App(){
+  const classes = useStyles();
+  return(<>
+    <Button className={classes.buttonStyle}>Small button</Button>
+    </>);
 };
-
-export default App;
